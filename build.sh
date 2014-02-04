77217003aa1acb200df1d49d40549b94c266aca1
Cymen Vig <cymenvig@gmail.com>
2014-02-04 02:03:21 -0600

    Use -O2 for compilation

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-emcc negamax.c -o module.js -s EXPORTED_FUNCTIONS="['_negamax']" && echo "" >> module.js && echo "module.exports = Module;" >> module.js
+emcc -O2 negamax.c -o module.js -s EXPORTED_FUNCTIONS="['_negamax']" && echo "" >> module.js && echo "module.exports = Module;" >> module.js
